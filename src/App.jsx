635482e72b710eb595d654430efb4e5bf62cf331
fefe4 <mynameisfefe@protonmail.com>
2023-08-24 18:58:53 -0300

    created template for website

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <main>
+      <section>
+        <h1>About me</h1>
+        <p></p>
+      </section>
+      <section className=''>
+        <h2>Projects</h2>
+        <p></p>
+      </section>
+    </main>
   )
 }
+
+
 
 export default App
