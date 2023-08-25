33006d36b277bd0a5a1711cb4e60aaa3e9916cde
fefe4 <mynameisfefe@protonmail.com>
2023-08-24 22:08:45 -0300

    template for project card

---
+++ src/Project.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Project = ({
+
+}) => (
+    <div>
+        <Carousel>
+            <div>
+                <img src="image1.jpg" alt="Image 1" />
+                <p className="legend">Image 1</p>
+            </div>
+            <div>
+                <img src="image2.jpg" alt="Image 2" />
+                <p className="legend">Image 2</p>
+            </div>
+            {/* Add more slides here */}
+        </Carousel>
+        <a href="">Repository Link</a>
+        <a href="">Website's page</a>
+    </div>
+
+)
+
